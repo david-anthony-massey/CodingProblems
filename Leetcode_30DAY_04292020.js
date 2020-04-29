863974b0b83c09ed514342d6c11662d5d26905e4
David M <reku68@msn.com>
2020-04-29 14:23:18 -0500

    Pass Test 1

--- Leetcode_30DAY_04292020.js
+++ Leetcode_30DAY_04292020.js
@@ -13,75 +13,113 @@
  * @return {number}
  */
 
-var recursiveCheckL = function(root, currentPartialPath, maxPath) {
-    if (root.right === null && root.left === null) {
-     currentPartialPath = Math.max(currentPartialPath + root.val, root.val);   
-        
-    if(maxPath < currentPartialPath) {
-        maxPath = currentPartialPath;
-    }   
-        
-     return currentPartialPath;
- } else if(root.left === null) {
-     return recursiveCheckR(root.right, currentPartialPath, maxPath);
- } else if(root.right === null) {
-     return recursiveCheckL(root.left, currentPartialPath, maxPath);
- } else {
-     return Math.max(recursiveCheckL(root.left, currentPartialPath, maxPath), recursiveCheckR(root.right, currentPartialPath, maxPath));
- }
-          
-}
-
-var recursiveCheckR = function(root, currentPartialPath, maxPath) {
-   if (root.right === null && root.left === null) {
-     currentPartialPath = Math.max(currentPartialPath, root.val);   
-        
-    if(maxPath < currentPartialPath) {
-        maxPath = currentPartialPath;
-    }   
-        
-     return currentPartialPath;
- } else if(root.left === null) {
-     return recursiveCheckR(root.right, currentPartialPath, maxPath);
- } else if(root.right === null) {
-     return recursiveCheckL(root.left, currentPartialPath, maxPath);
- } else {
-     return Math.max(recursiveCheckL(root.left, currentPartialPath, maxPath), recursiveCheckR(root.right, currentPartialPath, maxPath));
- }
-
-}
-
-   
-var recursiveCheckLR = function(root, currentPartialPath, maxPath) {
- if (root.right === null && root.left === null) {
-     return maxPath;
- } else if(root.left === null) {
-     return recursiveCheckR(root.right, currentPartialPath, maxPath);
- } else if(root.right === null) {
-     return recursiveCheckL(root.left, currentPartialPath, maxPath);
- } else {
-     let leftPath = recursiveCheckL(root.left, currentPartialPath, maxPath);
-     let rightPath = recursiveCheckR(root.right, currentPartialPath, maxPath);
-     
-     if (leftPath < leftPath + rightPath && rightPath < leftPath + rightPath)
-         if (maxPath < leftPath + rightPath) {
-             maxPath = leftPath + rightPath;
-             return maxPath;
-         } else {
-             if (Math.max(leftPath,rightPath) > maxPath){
-                 return Math.max(leftPath,rightPath);
-             }
-             
-             return maxPath;
-         }
- }     
-}
-
 var maxPathSum = function(root) {
+    var maxPath = root.val;
+
+
+    var recursiveCheckLR = function(root, currentPartialPath) {
+        if (root.right === null && root.left === null) {
+            return maxPath;
+        } else if(root.left === null) {
+            return recursiveCheckR(root.right, currentPartialPath);
+        } else if(root.right === null) {
+            return recursiveCheckL(root.left, currentPartialPath);
+        } else {
+            let leftPath = recursiveCheckL(root.left, currentPartialPath);
+            let rightPath = recursiveCheckR(root.right, currentPartialPath);
+            
+            if (leftPath < leftPath + rightPath && rightPath < leftPath + rightPath)
+                if (maxPath < leftPath + rightPath) {
+                    maxPath = leftPath + rightPath;
+                    return maxPath;
+                } else {
+                    if (Math.max(leftPath,rightPath) > maxPath){
+                        return Math.max(leftPath,rightPath);
+                    }
+                    
+                    return maxPath;
+                }
+        }     
+       }
+
+    var recursiveCheckL = function(root, currentPartialPath) {
+        if (root.right === null && root.left === null) {
+            if(currentPartialPath + root.val < root.val) {
+                if(maxPath < root.val) {
+                    maxPath = root.val;
+                } 
+                return root.val;
+            }
+         currentPartialPath = Math.max(currentPartialPath + root.val, root.val);   
+            
+        if(maxPath < currentPartialPath) {
+            maxPath = currentPartialPath;
+        }   
+            
+         return currentPartialPath;
+     } else if(root.left === null) {
+         return recursiveCheckR(root.right, currentPartialPath);
+     } else if(root.right === null) {
+         return recursiveCheckL(root.left, currentPartialPath);
+     } else {
+        let leftPath = recursiveCheckL(root.left, currentPartialPath);
+        let rightPath = recursiveCheckR(root.right, currentPartialPath);
+        
+        if (leftPath < leftPath + rightPath && rightPath < leftPath + rightPath)
+            if (maxPath < leftPath + rightPath) {
+                maxPath = leftPath + rightPath;
+                return maxPath;
+            } else {
+                if (Math.max(leftPath,rightPath) > maxPath){
+                    return Math.max(leftPath,rightPath);
+                }
+                
+                return maxPath;
+            }
+    }
+              
+    }
+    
+    var recursiveCheckR = function(root, currentPartialPath) {
+        if (root.right === null && root.left === null) {
+            if(currentPartialPath + root.val < root.val) {
+                if(maxPath < root.val) {
+                    maxPath = root.val;
+                } 
+                return root.val;
+            }
+         currentPartialPath = Math.max(currentPartialPath + root.val, root.val);   
+            
+        if(maxPath < currentPartialPath) {
+            maxPath = currentPartialPath;
+        }   
+            
+         return currentPartialPath;
+     } else if(root.left === null) {
+         return recursiveCheckR(root.right, currentPartialPath);
+     } else if(root.right === null) {
+         return recursiveCheckL(root.left, currentPartialPath);
+     } else {
+
+        let leftPath = recursiveCheckL(root.left, currentPartialPath);
+            let rightPath = recursiveCheckR(root.right, currentPartialPath);
+            
+            if (leftPath < leftPath + rightPath && rightPath < leftPath + rightPath)
+                if (maxPath < leftPath + rightPath) {
+                    maxPath = leftPath + rightPath;
+                    return maxPath;
+                } else {
+                    if (Math.max(leftPath,rightPath) > maxPath){
+                        return Math.max(leftPath,rightPath);
+                    }
+                    
+                    return maxPath;
+                }
+        }
+    
+    }
   
-  var maxPath = root.val;
-  
-  return recursiveCheckLR(root, root.val, maxPath);
+  return recursiveCheckLR(root, root.val);
   
   
   
@@ -95,6 +133,6 @@ var tree = function TreeNode(val, left, right) {
     this.right = (right===undefined ? null : right)
 }
 
-var testTree = new tree(1, new tree(2), new tree(3));
+var testTree = new tree(-10, new tree(9), new tree(20, new tree(15), new tree(7)));
 
 console.log(maxPathSum(testTree));
